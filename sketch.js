0c8e6fa46330c1a2980131645d39e483cee1abff
Bretelle2 <43404065+Bretelle2@users.noreply.github.com>
2018-11-28 12:25:16 +0100

    meh

--- sketch.js
+++ sketch.js
@@ -1,11 +1,119 @@
-function preload(){
-  // put preload code here
+var song;
+var fft;
+var button;
+var w;
+var nPunte = 16;
+
+var strofa1 = ["Now", "the", "evening", "has", "come", "to", "a", "close", "And", "I've", "had", "my", "last", "dance", "with", "you", "So", "on", "to", "the", "empty", "streets", "we", "go", "And", "it", "might", "be", "my", "last", "chance", "with", "you", "So", "I", "might", "as", "well", "get", "it", "over", "with", "The",  "things", "I", "have", "to", "say",  "Won't",  "wait",   "until",    "another",     "day"]
+var intermezzo = ["Shut", "up", "Whoooooo", "Whoooooo", "Whoooooo"]
+var strofa2 = ["Check", "this", "out", "The", "Fabulous", "structure", "that's", "coaxed", "out", "of", "rubbles", "puddles", "splash", "Mechanisms", "burn", "with", "beeping", "sounds", "that", "own", "their", "humans", "sold", "as", "Ruthless", "rounds", "of", "radio", "dust, ", "Cranial", "mush, ", "men", "get", "flattened", "out", "Radials", "spun", "on", "dusted", "combatant", "joust", "After", "house", "of", "the", "dead", "heads", "fled, ", "it's", "just", "the", "city", "moans", "Malignant", "kid", "in", "it", "with", "sentence", "of", "sinister", "conferred", "Magna-funk", "asbestos, ", "the", "best", "at", "closed-quarters", "shit", "Some", "will", "gravitate", "to", "the", "falcon", "and", "burn", "in", "wordlessness", "Hangin'", "with", "the", "herd", "is", "my", "joy", "We", "buoy", "ordertake", "employ", "When", "the", "farmers", "feed", "the", "murder", "rate", "ploy", "We", "stow", "collected", "rebel", "factions", "in", "dirt", "and", "just", "follow", "The", "citizens", "all", "love", "to", "be", "loved, ", "we", "just", "follow", "Figure", "they", "ate", "the", "kids, ", "homey", "So", "fuck", "em", "save", "the", "adults", "Kids", "are", "patriotic, ", "robotic, ", "operate", "catapults", "And", "goose-step", "over", "innocence", "Vagrant", "of", "Reaganomics", "phasing", "Read", "the", "books", "that", "will", "burn", "at", "the", "barn", "raising", "(Independent", "like", "a", "fuck)", "(Oh, ", "Jesus)", "You", "misinterpreted", "that", "Funcrush", "shit", "So", "man, ", "funcrush", "this", "Spectacular", "dawning", "of", "the", "heardthinner", "faction", "where", "distraction", "is", "bliss", "Tyrell", "took", "a", "sabatical", "but", "back", "for", "the", "new", "semester", "(rockin", "that)", "Class", "action", "suit", "from", "the", "Laundromat", "of", "velour", "and", "pressed", "polyester", "American", "history", "exo-bytes", "cypher", "with", "the", "tainted", "offspring, ", "gimme", "nodos", "quick", "You", "need", "to", "haul", "that", "mega-dumb", "style", "to", "the", "antique", "roadshow, ", "bitch", "The", "system", "bleeds", "for", "the", "radio", "angry, ", "rock", "that", "wound", "aesthetic", "The", "name", "of", "this", "routine", "is", "live", "at", "man", "you", "just", "don't", "get", "it", "Please", "try", "to", "compartmentalize", "my", "dick", "With", "a", "little", "bit", "a", "that", "bitch", "hubris", "When", "the", "ritalin", "starts", "to", "fade", "I", "might", "get", "lucid", "Every", "mindfuck", "I", "handout", "comes", "with", "a", "free", "month", "of", "internet", "access", "And", "an", "updated", "year", "2003", "version", "of", "the", "mega", "clapper", "(you", "know", "the", "drill, ", "clap", "off)", "This", "is", "the", "third", "installment", "of", "a", "prequel", "that", "was", "never", "written", "right", "Filmed", "with", "that", "classic", "Brooklyn", "magic, ", "without", "Lucasarts", "graphics", "Rendered", "cuddly", "comic", "relief", "creatures", "or", "terrible", "child", "actors", "Get", "off", "that", "jade", "elephant, ", "you're", "stoned, ", "and", "remember", "everything", "backwards"]
+function cambiaSong() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
+
+function preload() {
+  song = loadSound('01 Fantastic Damage.mp3');
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  // createCanvas(256, 256);
+  colorMode(RGB);
+  angleMode(DEGREES);
+  button = createButton('play/stop');
+  button.mousePressed(cambiaSong);
+  song.play();
+  fft = new p5.FFT(0.9, 128);
+  w = width/64;
+
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  var spectrum = fft.analyze();
+  noStroke();
+  translate(width / 2, height / 2);
+  fill(255, 255, 255, 0)
+  stroke(255, 255, 255, 75)
+  text(frameCount, 0, 13)
+
+
+
+if (frameCount<4000) {
+  for (var i = 0; i < spectrum.length; i++) {
+    var angle = map(i, 0, spectrum.length, 0, 360*nPunte);
+    var amp = spectrum[i];
+    var y = map (amp, 0, 256, height, 0);
+    // rect(i*w, y, w, height - y);
+    var r = map(amp, 0, 256, 0, 100);
+    var x = r * cos(angle-frameCount/6);
+    var y = r * sin(angle-frameCount/3);
+    // stroke(i, 255, 255);
+    line(0, 0, x, y);
+    }
+  }
+
+  if (frameCount<4000) {
+    for (var i = 0; i < spectrum.length; i++) {
+      var angle = map(i, 0, spectrum.length, 0, 360*nPunte);
+      var amp = spectrum[i];
+      var y = map (amp, 0, 256, height, 0);
+      // rect(i*w, y, w, height - y);
+      var r = map(amp, 0, 256, 0, 100);
+      var x = r * cos(angle-frameCount/6);
+      var y = r * sin(angle-frameCount/3);
+      // stroke(i, 255, 255);
+      line(random(0,amp/20), random(0,amp/20), x, y);
+      }
+    }
+    // if (frameCount<4000) {
+    //   for (var i = 0; i < spectrum.length; i++) {
+    //     var angle = map(i, 0, spectrum.length, 0, 360*nPunte);
+    //     var amp = spectrum[i];
+    //     var y = map (amp, 0, 256, height, 0);
+    //     // rect(i*w, y, w, height - y);
+    //     var r = map(amp, 0, 256, 0, 100);
+    //     var x = r * cos(angle-frameCount/6);
+    //     var y = r * sin(angle-frameCount/3);
+    //     // stroke(i, 255, 255);
+    //     line(-10, -10, x, y);
+    //     }
+    //   }
+    stroke(255,85)
+    for (var i = 0; i < spectrum.length; i++) {
+      var angle = map(i, 0, spectrum.length, 0, 360*amp);
+      var amp = spectrum[i];
+      var y = map (amp, 0, 256, height, 0);
+      // rect(i*w, y, w, height - y);
+      var r = map(amp, 0, 256, 0, 100);
+      var x = r * cos(angle);
+      var y = r * sin(angle);
+      // stroke(i, 255, 255);
+      line(x*4, y*4, x*3.8, y*3.8);
+      }
+      //comparsa testo
+      if (frameCount > 690 && frameCount < 950) {
+      for (var i = 0; i < 8; i++) {
+        text(strofa1[i], -width/2 + 13, (-height/2 + 13)+(13*i) )
+      }
+    }
+      if (frameCount > 960 && frameCount < 1000) {
+      for (var i = 9; i < 16; i++) {
+        text(strofa1[i], -width/2 + 13, (-height/2 + 13)+(13*i) )
+      }
+    }
+
+    if (frameCount > 1000 && frameCount < 1400) {
+    for (var i = 9; i < 16; i++) {
+      text(strofa1[i], -width/2 + 13, (-height/2 + 13)+(13*i) )
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
